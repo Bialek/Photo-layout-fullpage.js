b20d786228943de98531ecc4fe9eacd1cebd1697
Rafał Białek <bialekrafal94@gmail.com>
2018-04-25 16:43:37 +0200

    add section3

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,70 @@
 $(document).ready(function() {
-			$('#fullpage').fullpage({
-				anchors: ['firstPage', 'secondPage', '3rdPage', '4thPage', '5thPage', '6thPage'],
+	$('#fullpage').fullpage({
+		anchors: ['firstPage', 'secondPage', '3rdPage', '4thPage', '5thPage', '6thPage'],
 
-				navigation: true,
-				navigationPosition: 'right',
-				navigationTooltips: ['Home', 'Photo App', 'Design', 'Community', 'Subscibe', 'Download'],
-				responsiveWidth: 786,
-				afterResponsive: function(isResponsive){
-					
-				}
+		navigation: true,
+		navigationPosition: 'right',
+		navigationTooltips: ['Home', 'Photo App', 'Design', 'Community', 'Subscibe', 'Download'],
+		responsiveWidth: 786,
+		afterResponsive: function(isResponsive){
+			
+		}
 
-			});
+	});
+});
+
+$(document).ready(function() {
+	$('#fullpage').fullpage({
+		verticalCentered: false,
+
+	});
+});
+
+
+$(function(){
+	var list = $('#textSlider ul'),
+		sliderCount = list.find('li').toArray().length,
+		numberSlide = 1;
+
+	slideNumber();
+
+		
+
+
+	$('#section2 #right').click(function(){
+		list.animate({"marginLeft":-400}, 500, function(){
+			var firstItem = list.find('li:first'),
+				lastItem = list.find('li:last');
+			lastItem.after(firstItem);
+			list.css({marginLeft:0});
 		});
+		numberSlide++;
+		slideNumber();
+	});
 
-		$(document).ready(function() {
-			$('#fullpage').fullpage({
-				verticalCentered: false,
+	$('#section2 #left').click(function(){
+		list.animate({"marginLeft":400}, 500, function(){
+			for (var i = 0; i < (sliderCount-1); i++){
 
-			});
+				var firstItem = list.find('li:first'),
+				lastItem = list.find('li:last');
+				lastItem.after(firstItem);
+				list.css({marginLeft:0});
+			}
+			
 		});
+		numberSlide--;
+		slideNumber();
+	});
+	function slideNumber() {
+		if (numberSlide > sliderCount) {
+			numberSlide = 1;
+		} 
+		if (numberSlide < 1) {
+			numberSlide = 5;
+		} 
+		$('#control h2').text('0' +numberSlide+ '/0'+sliderCount);
+		
+	}
+	
+});
